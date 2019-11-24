bb4fb1fd3be6d9975f8a3ee5af736280256d4e71
Artem Torubarov <artem.torubarov@t-systems.com>
2019-11-24 22:58:19 +0300

    add persistent counter and answers

--- src/App.js
+++ src/App.js
@@ -1,40 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import connect from '@vkontakte/vk-connect';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
-import '@vkontakte/vkui/dist/vkui.css';
+import React, { useState, useEffect } from "react";
+import connect from "@vkontakte/vk-connect";
+import View from "@vkontakte/vkui/dist/components/View/View";
+import ScreenSpinner from "@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner";
+import "@vkontakte/vkui/dist/vkui.css";
 
-import Home from './panels/Home';
-import Ask from './popouts/Ask';
+import Home from "./panels/Home";
+import Ask from "./popouts/Ask";
 
 const App = () => {
-	const [fetchedUser, setUser] = useState(null);
-	const [question, setQuestion] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+  const maxAttempts = 10;
+  const [left, setLeft] = useState(maxAttempts);
+  const [userInfo, setUserInfo] = useState(null);
+  const [question, setQuestion] = useState(null);
+  const [popout, setPopout] = useState(<ScreenSpinner size="large" />);
+  const retry = () => {
+    setQuestion(null);
+  };
+  const updateInfo = () => {
+    const newInfo = {
+      count: userInfo.count + 1,
+      lastDate: new Date()
+    };
+    connect
+      .sendPromise("VKWebAppStorageSet", {
+        key: "count",
+        value: String(newInfo.count)
+      })
+      .then(
+        connect.sendPromise("VKWebAppStorageSet", {
+          key: "lastDate",
+          value: String(newInfo.lastDate)
+        })
+      )
+      .then(res => {
+        setUserInfo(newInfo);
+      });
+  };
 
-	useEffect(() => {
-		async function fetchData() {
-			// get last roll data here
-			const user = await connect.sendPromise('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-			// if user has no roll setPopout(pay)
-		}
-		fetchData();
-	}, []);
 
-	useEffect(() => {
-		if (question == null && fetchedUser != null) {
-			setPopout(<Ask question={question} setQuestion={setQuestion} setPopout={setPopout} />)
-		}
-	}, [fetchedUser, question]);
+  const debugSetAttemptsZero = () => {
+    const newInfo = {
+      count: 0,
+      lastDate: new Date()
+    };
+    connect
+      .sendPromise("VKWebAppStorageSet", {
+        key: "count",
+        value: String(newInfo.count)
+      })
+      .then(
+        connect.sendPromise("VKWebAppStorageSet", {
+          key: "lastDate",
+          value: String(newInfo.lastDate)
+        })
+      )
+      .then(res => {
+        setUserInfo(newInfo);
+      });
+  };
 
-	return (
-		<View activePanel="home" popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} question={question} retry={()=>setQuestion(null)}/>
-		</View>
-	);
-}
+  useEffect(() => {
+    async function fetchData() {
+      // get last roll data here
+      const response = await connect.sendPromise("VKWebAppStorageGet", {
+        keys: ["count", "lastDate"]
+      });
+      setPopout(null);
+      var info = { count: 0 };
+      console.log(response.keys);
+      console.log(info);
+      response.keys.map(e => {
+        switch (e.key) {
+          case "count":
+            info.count = Number(e.value);
+            break;
+          case "lastDate":
+            info.lastDate = Date(e.value);
+        }
+      });
+      console.log(info);
+      setUserInfo(info);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const countLeft =
+      userInfo == null ? maxAttempts : maxAttempts - userInfo.count;
+    setLeft(countLeft);
+    if (userInfo != null && countLeft <= 0) {
+      // TODO: if user has no roll setPopout(pay) - open payment dialog
+      alert("Please Pay");
+    }
+  }, [userInfo]);
+
+  useEffect(() => {
+    if (question == null && userInfo != null && userInfo.count < maxAttempts) {
+      setPopout(
+        <Ask
+          question={question}
+          setQuestion={setQuestion}
+          setPopout={setPopout}
+        />
+      );
+    }
+  }, [question, userInfo]);
+
+  return (
+    <View activePanel="home" popout={popout}>
+      <Home
+        id="home"
+        question={question}
+        left={left}
+        retry={retry}
+		updateInfo={updateInfo}
+		debugSetAttemptsZero={debugSetAttemptsZero}
+      />
+    </View>
+  );
+};
 
 export default App;
-
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,53 +1,89 @@
-import React, { useRef, useEffect } from "react"
-import PropTypes from 'prop-types';
-import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
-import './Home.css';
+import React, { useRef, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
+import Group from "@vkontakte/vkui/dist/components/Group/Group";
+import Div from "@vkontakte/vkui/dist/components/Div/Div";
+import "./Home.css";
 
-import videoSample from '../vid/ballDraft.webm';
+import videoSample from "../vid/ballDraft.webm";
 
-const Home = ({ id, fetchedUser, question, retry }) => {
-	const videoRef = useRef(null);
+const Home = ({
+  id,
+  left,
+  question,
+  retry,
+  updateInfo,
+  debugSetAttemptsZero
+}) => {
+  const answers = ["да", "нет", "хз"];
+  const [answer, setAnswer] = useState(null);
+  const [videoPlayed, setVideoPlayed] = useState(false);
+  const videoRef = useRef(null);
 
-	useEffect(() => {
-		if (question != null) {
-			videoRef.current.play()
-		}
-	}, [question]);
+  useEffect(() => {
+    if (question != null) {
+      videoRef.current.play();
+      updateInfo();
+    }
+  }, [question]);
 
-	return (
-		<Panel id={id}>
-			<Group title={question}>
-				<Div>
-					<video ref={videoRef} width="100%" height="auto">
-						<source src={videoSample} type="video/webm" />
-					</video>
-				</Div>
-				{question != null &&
-					<Div>
-						<Button size="xl" level="2" onClick={() => retry()}>
-							Еще Раз
-				</Button>
-					</Div>}
-			</Group>
-		</Panel>
-	);
-}
+  useEffect(() => {
+    if (answer == null) {
+      const num = Math.floor(Math.random() * answers.length);
+      setAnswer(answers[num]);
+    }
+  }, [answer, answers]);
+
+  return (
+    <Panel id={id}>
+      <Group title={"Осталось " + left + ". " + question}>
+        <Div className="video-container">
+          <video
+            ref={videoRef}
+            width="100%"
+            height="auto"
+            preload="auto"
+            onEnded={() => setVideoPlayed(true)}
+          >
+            <source src={videoSample} type="video/webm" />
+          </video>
+          {answer != null && videoPlayed && (
+            <Div className="overlay">{answer}</Div>
+          )}
+        </Div>
+
+        {question != null && (
+          <Div>
+            <Button
+              size="xl"
+              level="2"
+              onClick={() => {
+                setAnswer(null);
+                setVideoPlayed(false);
+                retry();
+              }}
+            >
+              Еще Раз
+            </Button>
+          </Div>
+        )}
+        <Div>
+          <Button size="xl" level="2" onClick={() => debugSetAttemptsZero()}>
+            Сбросить попытки
+          </Button>
+        </Div>
+      </Group>
+    </Panel>
+  );
+};
 Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-	question: PropTypes.string,
-	retry: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  left: PropTypes.number.isRequired,
+  question: PropTypes.string,
+  retry: PropTypes.func.isRequired,
+  updateInfo: PropTypes.func.isRequired,
+  debugSetAttemptsZero: PropTypes.func.isRequired
 };
 
 export default Home;
